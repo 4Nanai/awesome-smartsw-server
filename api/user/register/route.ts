ebbc60d0150c77a0e95f2e7da6e9386ba5fe7b6e
chitose <103087652@qq.com>
2025-11-01 23:03:20 -0400

    Refactor user registration route to improve variable naming for clarity

--- api/user/register/route.ts
+++ api/user/register/route.ts
@@ -7,17 +7,17 @@ import {ResultSetHeader} from "mysql2";
 const UserRegisterRouter = Router();
 
 UserRegisterRouter.post("/", async (req, res) => {
-    const userInfo: UserRegisterDTO = req.body;
-    if (!userInfo.email || !userInfo.username || !userInfo.password) {
+    const registerDTO: UserRegisterDTO = req.body;
+    if (!registerDTO.email || !registerDTO.username || !registerDTO.password) {
         res.status(400).json({message: "Missing required fields"});
         return;
     }
     // user password encryption
     try {
         const saltRound = 10;
-        const passwordHash = await bcrypt.hash(userInfo.password, saltRound);
+        const passwordHash = await bcrypt.hash(registerDTO.password, saltRound);
         const query = `INSERT INTO users (username, password_hash, email, created_at) VALUES (?, ?, ?, ?)`;
-        const [result] = await db.execute<ResultSetHeader>(query, [userInfo.username, passwordHash, userInfo.email, new Date()]);
+        const [result] = await db.execute<ResultSetHeader>(query, [registerDTO.username, passwordHash, registerDTO.email, new Date()]);
         res.status(201).json({
             message: "User registered successfully",
             userId: result.insertId,
